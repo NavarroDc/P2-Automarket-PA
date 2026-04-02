28033ea4bd73cef9038dc527084b9fe8f2f706d3
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-02 00:48:58 -0600

    clase vehiculo creada

---
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/Vehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Vehiculo
+    {
+        public int IdVehiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; } //Se utiliza "Anio" en vez de "Año" para evitar problemas de compatibilidad
+        public decimal Precio { get; set; }
+        public int IdCategoria { get; set; }
+        public char Estado { get; set; }
+
+        public Vehiculo(int idVehiculo, string marca, string modelo, int anio, decimal precio, int idCategoria, char estado)
+        {
+            IdVehiculo = idVehiculo;
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+            Precio = precio;
+            IdCategoria = idCategoria;
+            Estado = estado;
+        }
+    }
+}
