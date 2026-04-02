bcebbf2bae46c00db742d0640dc327f744c9c855
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-01 20:26:43 -0600

    CategoriaVehiculo entidad creada

---
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/CategoriaVehiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class CategoriaVehiculo
+    {
+        public int IdCategoria { get; set; }
+        public string NombreCategoria { get; set; }
+        public string Descripcion {  get; set; }
+
+        public CategoriaVehiculo(int idCategoria, string nombreCategoria, string descripcion)
+        {
+            IdCategoria = idCategoria;
+            NombreCategoria = nombreCategoria;
+            Descripcion = descripcion;
+        }
+
+        public CategoriaVehiculo(string nombreCategoria, string descripcion)
+        {
+            NombreCategoria = nombreCategoria;
+            Descripcion = descripcion;
+        }
+    }
+}
