1733eed90029a61140bf7b942e46ca804f14684b
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-03 14:24:06 -0600

    clase VehiculoXSucursal

---
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/VehiculoXSucursal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class VehiculoXSucursal
+    {
+        public int IdSucursal { get; set; }
+        public int IdVehiculo { get; set; }
+        public int cantidad { get; set; }
+    }
+}
