b1ad64219a12fa44b86243a46392c67e3f36bdc9
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-03 14:21:01 -0600

    clase Cliente creada

---
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public bool Activo { get; set; }
+
+        public Cliente(int idCliente, string identificacion, string nombreCompleto, DateTime fechaNacimiento, DateTime fechaRegistro, bool activo)
+        {
+            IdCliente = idCliente;
+            Identificacion = identificacion;
+            NombreCompleto = nombreCompleto;
+            FechaNacimiento = fechaNacimiento;
+            FechaRegistro = fechaRegistro;
+            Activo = activo;
+        }
+    }
+}
