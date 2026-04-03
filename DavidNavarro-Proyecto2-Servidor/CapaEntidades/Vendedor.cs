e5db723e047ba3091654a2448422659eade21ab9
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-03 14:16:36 -0600

    clase Vendedor creada

---
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/Vendedor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Vendedor
+    {
+        public int IdVendedor { get; set; }
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Telefono { get; set; }
+
+        public Vendedor(int idVendedor, string identificacion, string nombreCompleto, DateTime fechaNacimiento, DateTime fechaIngreso, string telefono)
+        {
+            IdVendedor = idVendedor;
+            Identificacion = identificacion;
+            NombreCompleto = nombreCompleto;
+            FechaNacimiento = fechaNacimiento;
+            FechaIngreso = fechaIngreso;
+            Telefono = telefono;
+        }
+    }
+}
