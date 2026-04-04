5c6067e8660cad2e3aa30e1e6a2f001f9c9fc541
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-04 10:59:20 -0600

    clase CrearConexionDatos creada

---
+++ DavidNavarro-Proyecto2-Servidor/CapaAccesoDatos/ConexionBaseDatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    public class ConexionBaseDatos
+    {
+        private string connectionString;
+
+        public ConexionBaseDatos()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["AutoMarketDB"].ConnectionString;
+        }
+
+        public SqlConnection CrearConexionDatos()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
