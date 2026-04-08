0f29c3ffbef75e133d5814c10c5cee8c9b6046d4
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-08 17:12:14 -0600

    conexión exitosa PRUEBA

--- DavidNavarro-Proyecto2-Servidor/ServidorTest/Program.cs
+++ DavidNavarro-Proyecto2-Servidor/ServidorTest/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.SqlClient;
+using CapaAccesoDatos;
+
+Console.WriteLine("INICIO DEL PROGRAMA");
+
+ConexionBaseDatos conexionDB = new ConexionBaseDatos();
+
+using (SqlConnection conexion = conexionDB.CrearConexionDatos())
+{
+    try
+    {
+        Console.WriteLine("ANTES DE ABRIR CONEXION");
+
+        conexion.Open();
+
+        Console.WriteLine("CONEXION EXITOSA ");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("ERROR: " + ex.Message);
+    }
+}
+
+Console.ReadLine();
