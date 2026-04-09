3d8381c1eb090205788c6f7b24e66f83393c7d9a
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-09 14:16:37 -0600

    ajuste clase vehiculo

--- DavidNavarro-Proyecto2-Servidor/CapaEntidades/Vehiculo.cs
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/Vehiculo.cs
@@ -13,17 +13,17 @@ namespace CapaEntidades
         public string Modelo { get; set; }
         public int Anio { get; set; } //Se utiliza "Anio" en vez de "Año" para evitar problemas de compatibilidad
         public decimal Precio { get; set; }
-        public int IdCategoria { get; set; }
+        public CategoriaVehiculo Categoria {  get; set; }
         public char Estado { get; set; }
 
-        public Vehiculo(int idVehiculo, string marca, string modelo, int anio, decimal precio, int idCategoria, char estado)
+        public Vehiculo(int idVehiculo, string marca, string modelo, int anio, decimal precio, CategoriaVehiculo categoria, char estado)
         {
             IdVehiculo = idVehiculo;
             Marca = marca;
             Modelo = modelo;
             Anio = anio;
             Precio = precio;
-            IdCategoria = idCategoria;
+            Categoria = categoria;
             Estado = estado;
         }
     }
