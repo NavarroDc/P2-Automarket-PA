53decbac6069bfafc5f9e1f68bbce7c3466a751d
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-09 14:27:28 -0600

    ajuste clase Sucursal

--- DavidNavarro-Proyecto2-Servidor/CapaEntidades/Sucursal.cs
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/Sucursal.cs
@@ -12,16 +12,20 @@ namespace CapaEntidades
         public string Nombre { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
-        public int IdVendedor { get; set; }
+        public Vendedor Vendedor { get; set; }
         public bool Activo { get; set; }
 
-        public Sucursal(int idSucursal, string nombre, string direccion, string telefono, int idVendedor, bool activo)
+        public Sucursal()
+        {
+        }
+
+        public Sucursal(int idSucursal, string nombre, string direccion, string telefono, Vendedor vendedor, bool activo)
         {
             IdSucursal = idSucursal;
             Nombre = nombre;
             Direccion = direccion;
             Telefono = telefono;
-            IdVendedor = idVendedor;
+            Vendedor = vendedor;
             Activo = activo;
         }
     }
