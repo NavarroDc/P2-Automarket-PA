6626f1ca7bd4328469d275849978f0afa220e918
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-04-10 08:26:48 -0600

    encabezados en clases entidades

--- DavidNavarro-Proyecto2-Servidor/CapaEntidades/CategoriaVehiculo.cs
+++ DavidNavarro-Proyecto2-Servidor/CapaEntidades/CategoriaVehiculo.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* UNED I Cuatrimestre
+ * Programación avanzada
+ * Profesor: Johan Figueroa Guevara
+ * Proyecto 2: Automarket
+ * Estudiante: David Navarro Camacho
+ * Fecha: 12/4/2026
+ */
+
 namespace CapaEntidades
 {
     public class CategoriaVehiculo
